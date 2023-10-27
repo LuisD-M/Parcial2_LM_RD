c7fa0c050c41d56c32ca2ff0c9636749b7d0015f
raul-daza <raul.daza@udea.edu.co>
2023-10-27 17:26:38 -0500

    "historial restaurado"

---
+++ historial.h
@@ -0,0 +1,22 @@
+#ifndef HISTORIAL_H
+#define HISTORIAL_H
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <QObject>
+
+
+using namespace std;
+
+class historial
+{
+
+public:
+    historial();
+
+    void VerResultados();
+
+};
+
+#endif // HISTORIAL_H
